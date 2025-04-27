70ea71eb058ea01ec9892c3fb2d3b7ee71a4f468
KanwalKhanbhu <kanwerkhanbhu8@gmail.com>
2025-04-27 15:08:00 +0000

    ss

--- ecommerce/Controllers/ProductsController.cs
+++ ecommerce/Controllers/ProductsController.cs
@@ -10,9 +10,114 @@ public class ProductsController : Controller
         _context = context;
     }
 
+    // GET: Products
     public IActionResult Index()
     {
-        var products = _context.Products.ToList(); // Example usage
+        var products = _context.Products.ToList();
         return View(products);
+    }
+
+    // GET: Products/Details/5
+    public IActionResult Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var product = _context.Products.FirstOrDefault(p => p.ProductId == id); // Use ProductId
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+
+    // GET: Products/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Products/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(product);
+    }
+
+    // GET: Products/Edit/5
+    public IActionResult Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var product = _context.Products.Find(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+
+    // POST: Products/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Product product)
+    {
+        if (id != product.ProductId) // Use ProductId
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(product);
+    }
+
+    // GET: Products/Delete/5
+    public IActionResult Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var product = _context.Products.FirstOrDefault(p => p.ProductId == id); // Use ProductId
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+
+    // POST: Products/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var product = _context.Products.Find(id);
+        if (product != null)
+        {
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+        }
+        return RedirectToAction(nameof(Index));
     }
 }
--- ecommerce/obj/Debug/net8.0/ecommerce.AssemblyInfo.cs
+++ ecommerce/obj/Debug/net8.0/ecommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c1c1981ddf10ed06725bbfa3fb77f099481004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f2eab0d8c469fa367d4345329c5ff58e5737ab0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
